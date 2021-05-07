cb382eaccd7ec16d79106c606b0af22bbbab2518
petran <petranb2@gmail.com>
2021-05-07 21:42:14 +0300

    Change at [...username] page - Add custom auth hook - minor changes to work both with client and server render

--- pages/[...username].tsx
+++ pages/[...username].tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { default as ProfileTemplate } from "../src/ui/templates/user/profile/profile"
 import { getUserProfileWithCookieCase } from "../src/core/user/case";
+import { useAuth } from "../src/ui/templates/user/hook/useAuth";
 
 function Profile({ profile }: any) {
+    useAuth();
     return <ProfileTemplate profile={profile} />
 }
 
@@ -11,14 +13,15 @@ export default Profile;
 // This gets called on every request
 export async function getServerSideProps(context: any) {
     // export the @ symbol from url path
-    let atSign = context.resolvedUrl[1];
+    let fullUsername = context.query.username[0]
+    let atSign = fullUsername[0];
     if (atSign !== '@') {
         return {
             notFound: true,
         }
     }
     // get the username after the @ symbol
-    let username = context.resolvedUrl.slice(2);
+    let username = fullUsername.slice(1);
     try {
         let userProfile = await getUserProfileWithCookieCase.execute(username, context.req.headers.cookie);
         return { props: { profile: userProfile } }
