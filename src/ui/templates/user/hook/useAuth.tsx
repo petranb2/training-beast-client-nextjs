5a86dd9133e5e8d5f8f8e02f5338c255ccc1ccc1
petran <petranb2@gmail.com>
2021-05-07 21:42:29 +0300

    Create custom auth hook

---
+++ src/ui/templates/user/hook/useAuth.tsx
@@ -0,0 +1,21 @@
+import { useContext, useEffect } from 'react';
+import { AuthContext } from "../context/authContext";
+import { checkUserCase } from "../../../../core/user/case";
+// Client Side Auth
+export const useAuth = () => {
+    const authContext = useContext(AuthContext);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let userProfile = await checkUserCase.execute();
+                authContext.updateState({ ...userProfile, isAuthChecked: true });
+            } catch (error) {
+                authContext.updateState({ isAuthChecked: true });
+            }
+        };
+        if (!authContext.isAuthChecked) {
+            fetchData();
+        }
+
+    }, [])
+}
